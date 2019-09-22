7961ada84c87dfe0304b84764f7e89d2e46b935e
181240020-Hu Junhao <1185276716@qq.com>
2019-09-22 11:26:23 +0800

    Add code: Call Expr() and the corresponding headers

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,5 +1,13 @@
+//This can be deleted because we are only using expr for checking purpose
+#include<stdlib.h>
+#include "nemu.h"
+#include<regex.h>
+#include<sys/types.h>
+#include "../include/monitor/expr.h"
+
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
+
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
@@ -7,6 +15,29 @@ int main(int argc, char *argv[]) {
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
+  
+	//To test the expression. This part can be deleted
+	uint32_t Result=0;
+ 	char buf[63356];
+	FILE *fp=fopen("/home/hjh/ics2019/nemu/src/tmp/input.txt","r");
+  bool Success=true;
+	if(fp==NULL)
+	{
+		printf("File open failed!!\n");
+		assert(0);
+	}
+	while(!feof(fp))
+	{
+		Success=fscanf(fp,"%u %s\n",&Result,buf);
+		printf("Read: %u %s\n",Result,buf);
+		printf("Ture result: %u    Calculated result:%u\n",Result,expr(buf,&Success));
+	}
+  fclose(fp);
+
+
+
+
+
 
   return 0;
 }
