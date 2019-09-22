c89aaeed3309de8b83fb1bcfd5e1377d74a0289a
181240020-Hu Junhao <1185276716@qq.com>
2019-09-22 11:29:01 +0800

    Realized generating random expressions

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -7,8 +7,57 @@
 
 // this should be enough
 static char buf[65536];
-static inline void gen_rand_expr() {
-  buf[0] = '\0';
+//Self added
+char *get_expr(int Depth)
+{
+	char *temp1=(char *)malloc(65536);
+	char *ans=(char *)malloc(65536);
+	if(Depth>3)
+	{
+		sprintf(ans,"%d",rand()%500);
+		return ans;
+	}
+	switch(rand()%3)
+	{
+		case 0:
+			sprintf(ans,"%d",rand()%500);
+			return ans;
+		case 1:
+			temp1=get_expr(Depth+1);
+    	ans[0]='(',ans[1]='\0';
+			strcat(ans,temp1);
+			strcat(ans,")");
+			return ans;
+		default:
+			ans=get_expr(Depth+1);
+			temp1=get_expr(Depth+1);
+			switch(rand()%4)
+			{
+				case 0:
+					strcat(ans,"+");
+					strcat(ans,temp1);
+					return ans;          
+				case 1:
+      		strcat(ans,"-");
+      		strcat(ans,temp1);
+      		return ans;       
+      	case 2:
+	      	strcat(ans,"*");
+	      	strcat(ans,temp1);
+	      	return ans;       
+      	default:
+	      	strcat(ans,"/");
+	      	strcat(ans,temp1);
+	      	return ans;       
+			}
+	}
+}
+
+static inline void gen_rand_expr() 
+{
+  buf[0]='\0';
+  strcat(buf,get_expr(0));	 
+  return;
 }
 
 static char code_buf[65536];
@@ -27,6 +76,15 @@ int main(int argc, char *argv[]) {
   if (argc > 1) {
     sscanf(argv[1], "%d", &loop);
   }
+
+ //Self added
+  FILE *haha=fopen("/home/hjh/ics2019/nemu/src/tmp/input.txt","w");
+  if(haha==NULL)
+  {
+   	printf("File open failed at gen-expr.c line %d\n",__LINE__);
+    assert(0);
+	}
+
   int i;
   for (i = 0; i < loop; i ++) {
     gen_rand_expr();
@@ -45,10 +103,19 @@ int main(int argc, char *argv[]) {
     assert(fp != NULL);
 
     int result;
-    fscanf(fp, "%d", &result);
+    //Self added if but fscanf is original
+    if(fscanf(fp, "%d", &result)==0)
+		{
+			printf("File read failed at gen-expr line %d\n",__LINE__);
+			assert(0);
+		}
+
     pclose(fp);
 
-    printf("%u %s\n", result, buf);
+		//Change printf to fprintf
+    fprintf(haha,"%u %s\n", result, buf);
   }
+
+	fclose(haha);//Self added
   return 0;
 }
